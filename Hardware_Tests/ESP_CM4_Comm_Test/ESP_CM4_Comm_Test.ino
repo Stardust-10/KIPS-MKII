cbcf8799deaecb740cbcc4210f7ec7982dd891b4
Stardust <122560405+Stardust-10@users.noreply.github.com>
2025-11-18 23:58:40 -0500

    Replaced temp + hum sensor test and modified ESP_CM4_Comm_Test
    The comm test does NOT work currently. The devices do NOT communicate w/ each w/ this code. Only the ESP32-S3 does writes anything.

--- Hardware_Tests/ESP_CM4_Comm_Test/ESP_CM4_Comm_Test.ino
+++ Hardware_Tests/ESP_CM4_Comm_Test/ESP_CM4_Comm_Test.ino
@@ -1,28 +1,36 @@
 // ESP32 TX and RX Pins
-#define ESP32_TX_PIN 37   // ESP32 TX -> Pi RX
-#define ESP32_RX_PIN 36   // ESP32 RX -> Pi TX
+#define ESP32_TX_PIN 37   // ESP32 TX (pin 37) -> Pi RX (pin 10)
+#define ESP32_RX_PIN 36   // ESP32 RX (pin 36) -> Pi TX (pin 8)
+
+#define ESP32_RTS_PIN 15 //ESP32 RTS (pin 15) -> Pi RTS (pin 11)
+#define ESP32_CTS_PIN 16 //ESP32 CTS (pin 16) -> Pi CTS (pin 36)
 
 void setup() {
-  Serial.begin(115200);           // Baud Rate
+  Serial.begin(115200);  // Baud Rate, Serial is also ESP32 -> Serial Monitor
+  Serial.println("Serial for ESP32 started.");
   delay(1000);
   Serial.println("Booting...");
 
-  // UART to Raspberry Pi CM4
+  // Tell Serial2 (CM4) which pins to use
+  Serial2.setPins(ESP32_TX_PIN, ESP32_RX_PIN, ESP32_CTS_PIN, ESP32_RTS_PIN);
+  Serial.println("Serial2 for Pi started.");
+
+  //Start UART, baud + 8N1
   Serial2.begin(115200, SERIAL_8N1, ESP32_RX_PIN, ESP32_TX_PIN);
-  Serial.println("Serial2 (to Pi) started.");
+  Serial2.println("Serial2 for Pi started w/ RTS/CTS.");
 }
 
 void loop() {
 
-  // Send a test message every second
-  Serial2.println("Hello from ESP32!");
-  Serial.println("Sent: Hello from ESP32!");
-  delay(1000);
+  //SERIAL IS FROM ESP
+  //SERIAL2 IS FROM CM4
 
-  // (Optional) Read anything the Pi sends back
-  while (Serial2.available()) {
-    int c = Serial2.read();
-    Serial.print("From Pi: ");
-    Serial.println((char)c);
-  }
+  // Send a test message every second
+  Serial.println("Sent from ESP32: Hello from ESP32 w/ RTS/CTS!");
+  delay(5000);
+
+  int c = Serial2.read();
+  Serial2.print("Sent from Pi: Hello from Pi CM4! ");
+  Serial2.println(c);
+  delay(5000);
 }
