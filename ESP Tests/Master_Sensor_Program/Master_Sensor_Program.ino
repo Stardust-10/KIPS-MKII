12fb50ba52e4ae7023d7b184e93e3728f6815c1d
Stardust <122560405+Stardust-10@users.noreply.github.com>
2026-02-28 21:00:36 -0500

    Added a new program file for the master sensor program for all sensors.

--- ESP Tests/Master_Sensor_Program/Master_Sensor_Program.ino
+++ ESP Tests/Master_Sensor_Program/Master_Sensor_Program.ino
@@ -6,9 +6,12 @@
 
 
 // ======================================================== //
-// UART setup
+
+// UART Variable Setup
 HardwareSerial SerialUART32_0(0); // UART0
-HardwareSerial SerialUART32_1(1); // UART0
+HardwareSerial SerialUART32_1(1); // UART1
+
+const int ldrPin = 4;
 
 #define UART_TX_PIN 43
 #define UART_RX_PIN 44
@@ -18,24 +21,30 @@ HardwareSerial SerialUART32_1(1); // UART0
 Adafruit_AM2320 am2320 = Adafruit_AM2320();
 
 void setup() {
-  //Serial.println("Beginning ESP32-S3 UART + AM2320 test.");
-  // USB Serial Monitor
+
+  // USB Serial Monitor output setup
   Serial.begin(115200);
   delay(2000);
   Serial.println("Beginning ESP32-S3 UART + AM2320 test.");
 
+  //Pin for LDR
+  // Set the pin mode (though analogRead works without explicit pinMode for ADC pins)
+  pinMode(ldrPin, INPUT);
+
   // UART0 to Raspberry Pi
   SerialUART32_0.begin(112500, SERIAL_8N1, UART_RX_PIN, UART_TX_PIN);
-  //Wire.begin(SDA,SCL);
   delay(1000);
+
   Serial.println("UART0 initialized on TX=43 RX=44 @ 112500");
   am2320.begin();
 
-  // Sensor init
+  // Temp + Humidity Sensor detection check
   if (!am2320.begin()) {
     Serial.println("AM2320 not detected!");
-  } else {
-    Serial.println("AM2320 ready.");
+  } 
+  
+  else {
+    Serial.println("AM230 ready.");
   }
 }
 
@@ -52,11 +61,14 @@ void loop() {
 
     int command = incoming.toInt();
 
+    //If integer 1 recieved from CM4,
+    //ESP signals sensor to send readings
+    //then prints results to serial monitor + UART print 
     if (command == 1) {
       float temp = am2320.readTemperature();
       float hum  = am2320.readHumidity();
 
-      // Send sensor data ONCE
+      //Send sensor data ONCE
       //Prints over UART
       SerialUART32_0.print("TEMP:");
       SerialUART32_0.println(temp);
@@ -70,6 +82,28 @@ void loop() {
       Serial.print("Hum: "); Serial.println(hum);
 
        delay(2000);
+    }
+
+    //If integer 2 is recieved from CM4
+    //Sends back LDR value (LUX)
+    if(command == 2) {
+      
+      int ldr = analogRead(ldrPin);
+      
+      //Print to UART first
+      SerialUART32_0.print("LDR: ");
+      SerialUART32_0.print(ldr);
+
+      //Then serial monitor
+      Serial.println("LDR data sent once.");
+      Serial.println(ldr);
+
+      delay(2000);
+    }
+
+    //For heartbeat monitor
+    if (command == 3) {
+
     }
     
     else {
