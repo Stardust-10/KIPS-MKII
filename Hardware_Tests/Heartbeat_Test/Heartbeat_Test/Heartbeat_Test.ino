587fd6c06c33327beeca454f7dbacc0a4f6c9b19
Stardust <122560405+Stardust-10@users.noreply.github.com>
2026-03-31 15:18:56 -0400

    Modified the HB program that was left to be the correct one..

--- Hardware_Tests/Heartbeat_Test/Heartbeat_Test/Heartbeat_Test.ino
+++ Hardware_Tests/Heartbeat_Test/Heartbeat_Test/Heartbeat_Test.ino
@@ -12,87 +12,24 @@
  * @url  https://github.com/DFRobot/DFRobot_Heartrate
  */
 #include "DFRobot_Heartrate.h"
-#define HEARTRATE_PIN 4
-#define SAMPLING 10
-xz
-//Beat detection parameters
-#define THRESHOLD 20
-#define BASE_ALPHA 5 //Baseline smoothing (% per sample)
+#define heartratePin 4
 
-//Acceptable beat interval range (ms)
-#define MIN_INTV 350 //~171 BPM upper bound
-#define MAX_INTV 2000 //~30 BPM lower bound
-
-int lastSample, lastBeat, baseline, bpm = 0;
-bool above = false;
+DFRobot_Heartrate heartrate(DIGITAL_MODE);   // ANALOG_MODE or DIGITAL_MODE
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-
-  analogReadResolution(12); //0-4095 on the ESP32
-  analogSetPinAttenuation(HEARTRATE_PIN, ADC_11db);
-
-  //Take samples to initialize a baseline
-  long sum = 0;
-  const int N = 200;
-  
-  for (int i = 0; i < N; i++) {
-    sum += analogRead(HEARTRATE_PIN);
-    delay(5);
-  } 
-
-  baseline = sum / N;
-
 }
 
 void loop() {
-
-  unsigned long now = millis();
-
-  if(now - lastSample < SAMPLING) {
-    return; //Wait until the next sample time
-  }
-
-  lastSample = now;
-
-  int raw = analogRead(HEARTRATE_PIN);
-
-  //Exponential moving average for baseline
-  baseline = (BASE_ALPHA * raw + (100 - BASE_ALPHA) * baseline) / 100;
-
-  int diff = raw - baseline; //Will be positive when above baseline
-
-  //Peak detection
-    //If it crosses ABOVE the threshold, its a "beat"
   
-  if(!above && diff > THRESHOLD) {
-    above = true;
-
-    if(lastBeat != 0) {
-      unsigned long interval = now - lastBeat;
-
-      if(interval > MIN_INTV && interval < MAX_INTV) {
-        bpm = 60000 / interval;
-      }
-    }
-    lastBeat = now;
+  uint8_t rateValue;
+  
+  heartrate.getValue(heartratePin);   // A1 foot sampled values
+  
+  rateValue = heartrate.getRate();   // Get heart rate value 
+  
+  if(rateValue)  {
+    Serial.println(rateValue);
   }
-
-  if(above && diff < 0) {
-    //if it falls back below the baseline -> end of beat window
-    above = false;
-  }
-
-  //Serial plotter output
-  Serial.print(raw);
-  Serial.print(" ");
-  Serial.print(baseline);
-  Serial.print(" ");
-  Serial.print(diff);
-  Serial.print(" ");
-  Serial.println(bpm);
-
-  delay(200);
-
+  delay(20);
 }
